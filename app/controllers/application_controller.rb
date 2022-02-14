a8eec330f5d4375bbf70ef5488551d49b5291cdb
Christine Contreras <christineec23@yahoo.com>
2022-02-14 16:10:36 -0600

    initial load

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    
+    def render_unprocessable_entity(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+      end
+end
